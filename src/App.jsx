dba80fc1ed4b392eaba56d1f6137e8044ec5bd61
chayan <admin@ditskill.com>
2023-06-24 11:43:13 +0600

    env file added for api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useRef, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const inputRef = useRef(null);
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchWeather = async () => {
+    const searchTerm = inputRef.current.value;
+
+    if (searchTerm) {
+      setLoading(true);
+
+      try {
+        const response = await axios.get(
+          `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=${searchTerm}`
+        );
+        setWeather(response.data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+
+      setLoading(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="flex items-center justify-between">
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="Enter Your Location"
+        className="text-xl border-b p-1 border-gray-200 font-semibold uppercase flex-1"
+      />
+      <button
+        onClick={fetchWeather}
+        className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600"
+      >
+        Search Location
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
